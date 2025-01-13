6fa0a2f859a22bec4cec3b6c9424094698dd2e6c
Miguel Araujo <38594993+Maikuh@users.noreply.github.com>
2025-01-13 17:43:14 -0400

    feat: implement "See more" button and improve API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,36 +2,52 @@
 import Catalog from '@/components/Catalog'
 import Header from '@/components/Header'
 import { fetchGames } from '@/services/games.service'
-import { useQuery } from '@tanstack/react-query'
+import { useInfiniteQuery } from '@tanstack/react-query'
 import { useSearchParams } from 'next/navigation'
 import LoadingSpinner from '../components/LoadingSpinner'
+import { useMemo } from 'react'
 
 export default function Home() {
   const searchParams = useSearchParams()
-  const { data, isLoading, isFetching, isError } = useQuery({
-    queryKey: ['games', searchParams.get('genre'), searchParams.get('page')],
-    queryFn: ({ queryKey }) =>
-      fetchGames({ genre: queryKey[1], page: queryKey[2] }),
-    placeholderData: (prev) => prev,
-  })
+  const { data, isLoading, isFetching, isError, hasNextPage, fetchNextPage } =
+    useInfiniteQuery({
+      queryKey: ['games', searchParams.get('genre')],
+      queryFn: ({ queryKey, pageParam }) =>
+        fetchGames({
+          genre: queryKey[1],
+          page: pageParam.toString(),
+        }),
+      initialPageParam: 1,
+      placeholderData: (prev) => prev,
+      getNextPageParam: (lastPage) =>
+        lastPage.currentPage === lastPage.totalPages
+          ? undefined
+          : lastPage.currentPage + 1,
+    })
+
+  const games = useMemo(() => data?.pages.flatMap((page) => page.games), [data])
 
   if (isLoading) return <LoadingSpinner />
 
-  if (isError || !data) return <p>Something went wrong</p>
+  if (isError || !data || !games) return <p>Something went wrong</p>
 
   return (
     <>
       {isFetching && <LoadingSpinner />}
       <div className="flex min-h-dvh flex-col items-center py-12 text-gray-medium mx-8 sm:mx-20 lg:mx-32 2xl:mx-60 3xl:mx-96">
         <Header
-          genres={data.availableFilters}
+          genres={data.pages[0].availableFilters}
           initialGenre={searchParams.get('genre') ?? ''}
         />
 
-        <Catalog games={data.games} />
+        <Catalog games={games} />
 
-        {data.currentPage < data.totalPages && (
-          <button className="bg-primary rounded-lg py-5 px-6 text-white self-start">
+        {hasNextPage && (
+          <button
+            className="bg-primary rounded-lg py-5 px-6 text-white self-start disabled:bg-gray-400 disabled:text-gray-100"
+            onClick={() => fetchNextPage()}
+            disabled={isFetching || !hasNextPage}
+          >
             SEE MORE
           </button>
         )}
